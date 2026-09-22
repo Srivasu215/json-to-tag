a377194e80c15552b6bbc5b1f11f2785998b73b2
keshavsoft <nknnkumar@yahoo.com>
2026-09-22 09:49:35 +0530

    src 4

--- samples/form/v6/index.js
+++ samples/form/v6/index.js
@@ -1,18 +1,15 @@
 import spec from "./spec.json" with { type: "json" };
-// import { buildSpec } from "../../../src/v40/index.js";
-const startFunc = async () => {
+
+const startFunc = () => {
     try {
-        const k1 = window.ks['json-to-dom'].buildSpecElement({ targetHtmlId: "form-container", inSpec: spec })
-        console.log("k1 : ", k1);
+        const createDomElement = window.ks.jsonToTag.buildSpecElement(spec);
+        console.log("createDomElement : ", createDomElement);
 
         const cont1 = document.getElementById("form-container");
-        cont1.append(k1);
-        // const k1 = buildSpec({
-        //     inSpec: spec
-        // });
+        cont1.append(createDomElement);
     } catch (err) {
         console.error("Failed to render v27 sample:", err);
     }
 };
 
-startFunc().then();
+startFunc();
